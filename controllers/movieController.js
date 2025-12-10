11c5057dcb28791c203fdf56d881f3a3e2065081
angelostori <angelostori13@gmail.com>
2025-12-10 18:25:41 +0100

    implement show method to receive movie and its reviews

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -8,15 +8,31 @@ const index = (req, res) => {
 
     connection.query(sql, (err, result) => {
         if (err) return res.status(500).json({ error: true, message: err.message })
-        console.log(result);
-
+        //console.log(result);
         return res.json(result);
     })
 }
 
 // show
 const show = (req, res) => {
-    res.send('show film with id: ' + req.params.id)
+    const id = Number(req.params.id)
+    const sql = 'SELECT * FROM movies WHERE id = ?'
+    const sqlReviews = 'SELECT id, name, vote, text, created_at, updated_at FROM reviews WHERE movie_id = ?'
+
+    connection.query(sql, [id], (err, result) => {
+        if (err) return res.status(500).json({ error: true, message: err.message })
+        console.log(result);
+        if (result.length === 0) return res.status(404).json({ error: true, message: 'Movie not found!' })
+
+        const movie = result[0]
+
+        connection.query(sqlReviews, [id], (errReview, resultReview) => {
+            if (errReview) return res.status(500).json({ error: true, message: err.message })
+            console.log(resultReview);
+            movie.reviews = resultReview
+            res.json(movie)
+        })
+    })
 }
 
 module.exports = {
