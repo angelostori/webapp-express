e2fae16e0e7a0bbb00e624551197958f4b9d02e2
angelostori <angelostori13@gmail.com>
2025-12-10 16:10:59 +0100

    install dotenv to fix the .env file import dotenv on server.js, add the parses json and declaire public folder as public

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const db = require('./database/connection'); // importa la connessione
+const connection = require('./database/connection'); // importa la connessione
 const PORT = 3000
+
+app.use(express.json)
+
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.send('Server attivo!');
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log(`Server avviato su http://localhost:${PORT}`);
 });
